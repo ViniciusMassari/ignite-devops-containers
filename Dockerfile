dd8ef265d8cd6af507b44ab147c986e5d071c1cd
Vinícius Massari <stonefields11@hotmail.com>
2024-10-21 16:05:57 -0300

    conectando BD

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20-slim
+FROM node:20 AS build
 
 ENV PNPM_HOME="/pnpm"
 ENV PATH="$PNPM_HOME:$PATH"
@@ -13,7 +13,15 @@ RUN pnpm install
 COPY . .
 
 RUN pnpm run build
+RUN pnpm install --prod && pnpm store prune
+
+FROM node:20-alpine3.20
+
+WORKDIR /usr/src/app
+
+COPY --from=build /usr/src/app/dist ./dist
+COPY --from=build /usr/src/app/node_modules ./node_modules
 
 EXPOSE 3000
 
-CMD ["pnpm", "run", "start"]
+CMD ["node", "dist/main"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'admin',
+      password: 'root',
+      database: 'rocketseat-db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
